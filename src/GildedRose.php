04e695250b0ced32cbce92e6e298fa96f33f99b8
Magdalena Tubis <magda.tubis@gmail.com>
2024-11-07 22:19:59 +0100

    Function Extraction

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -10,55 +10,72 @@ final class GildedRose
     private const BACKSTAGE_PASS_FIRST_THRESHOLD = 10;
     private const BACKSTAGE_PASS_SECOND_THRESHOLD = 5;
 
-    public function updateQuality($item)
+    public function updateQuality(Item $item)
     {
-        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > self::MIN_QUALITY) {
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality -= 1;
-                } else {
-                    $item->quality = self::LEGENDARY_QUALITY;
-                }
-            }
-        } else {
-            if ($item->quality < self::MAX_QUALITY) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in <= self::BACKSTAGE_PASS_FIRST_THRESHOLD) {
-                        if ($item->quality < self::MAX_QUALITY) {
-                            $item->quality += 1;
-                        }
-                    }
-                    if ($item->sell_in <= self::BACKSTAGE_PASS_SECOND_THRESHOLD) {
-                        if ($item->quality < self::MAX_QUALITY) {
-                            $item->quality += 1;
-                        }
-                    }
-                }
-            }
+        if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+            return; // Legendary items do not require an update
         }
 
-        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-            $item->sell_in -= 1;
+        if ($item->name == 'Aged Brie') {
+            $this->updateAgedBrieQuality($item);
+        } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+            $this->updateBackstagePassQuality($item);
+        } else {
+            $this->updateStandardItemQuality($item);
         }
+
+        $this->updateSellIn($item);
 
         if ($item->sell_in < 0) {
-            if ($item->name != 'Aged Brie') {
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > self::MIN_QUALITY) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality -= 1;
-                        }
-                    }
-                } else {
-                    $item->quality = self::MIN_QUALITY;
-                }
-            } else {
-                if ($item->quality < self::MAX_QUALITY) {
-                    $item->quality += 1;
-                }
+            $this->applyQualityAfterExpiration($item);
+        }
+    }
+
+    private function updateAgedBrieQuality(Item $item)
+    {
+        if ($item->quality < self::MAX_QUALITY) {
+            $item->quality += 1;
+        }
+    }
+
+    private function updateBackstagePassQuality(Item $item)
+    {
+        if ($item->quality < self::MAX_QUALITY) {
+            $item->quality += 1;
+
+            if ($item->sell_in <= self::BACKSTAGE_PASS_FIRST_THRESHOLD && $item->quality < self::MAX_QUALITY) {
+                $item->quality += 1;
+            }
+            if ($item->sell_in <= self::BACKSTAGE_PASS_SECOND_THRESHOLD && $item->quality < self::MAX_QUALITY) {
+                $item->quality += 1;
             }
         }
     }
 
+    private function updateStandardItemQuality(Item $item)
+    {
+        if ($item->quality > self::MIN_QUALITY) {
+            $item->quality -= 1;
+        }
+    }
+
+    private function updateSellIn(Item $item)
+    {
+        $item->sell_in -= 1;
+    }
+
+    private function applyQualityAfterExpiration(Item $item)
+    {
+        if ($item->name == 'Aged Brie') {
+            if ($item->quality < self::MAX_QUALITY) {
+                $item->quality += 1;
+            }
+        } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+            $item->quality = self::MIN_QUALITY;
+        } else {
+            if ($item->quality > self::MIN_QUALITY) {
+                $item->quality -= 1;
+            }
+        }
+    }
 }
